861434a187efea3d929b05040bdf6b029579af26
abedie-saegus <charles.abedie@saegus.com>
2017-05-27 03:09:42 +0200

    webpack config dev and prod

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,58 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+// App Directory
+const srcDir = resolve(__dirname, '../src');
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'standard-loader',
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      
+      {
+        test: /\.scss|css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]____[hash:base64:5]',
+              camelCase: true
+            }
+          },{
+            loader: 'sass-loader'
+          }]
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${srcDir}/index.html`,
+    }),
+    new ExtractTextPlugin('style.css'),
+    new DashboardPlugin()
+  ]
+}
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,69 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const OfflinePlugin = require('offline-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  context: resolve(__dirname, '../src'),
+  entry: {
+    app: `./index.js`,
+    vendor: ['react', 'react-dom', 'react-router']
+  },
+  output: {
+    path: resolve(__dirname, '../dist'),
+    filename: '[name].[chunkhash:6].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      
+      {
+        test: /\.scss|css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]____[hash:base64:5]',
+              camelCase: true
+            }
+          },{
+            loader: 'sass-loader'
+          }]
+        }),
+      }
+    ]
+  },
+  devtool: 'source-map',
+  performance: {
+    hints: 'error'
+  },
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: resolve(__dirname, '..')
+    }),
+    new HtmlWebpackPlugin({
+      template: `./index.html`,
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin('styles.[chunkhash:6].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new OfflinePlugin()
+  ]
+}
